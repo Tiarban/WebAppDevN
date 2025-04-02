dd1554687aa2d934332d8410ad3fe93e14ba6b8f
Mahmood <mahmood.albadaai2@mail.dcu.ie>
2025-04-02 19:03:39 +0100

    The Initial Django project

---
+++ core/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='home'),
+    path('manager/', views.manager, name='manager'),
+    path('assgin-tech/', views.assign_tech, name='assgin-tech'),
+    path('raise-ticket/', views.raise_ticket, name='raise-ticket'),
+
+]
