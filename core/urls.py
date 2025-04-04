bbbcb5b80696cb76ab23c083df7f0f3e04a3b7e5
naveenprasathk2 <naveenprasath047@gmail.com>
2025-04-04 16:10:14 +0100

    Added homepage.html and images, logo and icons folder under core

--- core/urls.py
+++ core/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
+    path('homepage/', views.homepage, name='homepage'),
     path('manager/', views.manager, name='manager'),
     path('assgin-tech/', views.assign_tech, name='assgin-tech'),
     path('raise-ticket/', views.raise_ticket, name='raise-ticket'),
--- core/views.py
+++ core/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 def index(request): return render(request, 'core/navbar.html')
+def homepage(request): return render(request, 'core/homepage.html')
 def manager(request): return render(request, 'core/manager.html')
 def assign_tech(request): return render(request, 'core/assign-tech.html')
 def raise_ticket(request): return render(request, 'core/raise-ticket.html')
