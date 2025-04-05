f46029dda146459dbc4835a5a8d33bd151c10424
Mahmood <mahmood.albadaai2@mail.dcu.ie>
2025-04-05 13:22:49 +0100

    fixed some broken links

--- core/urls.py
+++ core/urls.py
@@ -2,11 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home'),
-    path('homepage/', views.homepage, name='homepage'),
+    #path('', views.index, name='home'),
+    path('', views.homepage, name='homepage'),
     path('manager/', views.manager, name='manager'),
-     path('machinedetails/', views.machinedetails, name='machinedetails'),
+    path('machinedetails/', views.machinedetails, name='machinedetails'),
     path('assgin-tech/', views.assign_tech, name='assgin-tech'),
     path('raise-ticket/', views.raise_ticket, name='raise-ticket'),
+    path('machines/<int:machine_id>/', views.machine_detail, name='machine-detail'),
+
 
 ]
--- core/views.py
+++ core/views.py
@@ -6,6 +6,7 @@ def manager(request): return render(request, 'core/manager.html')
 def machinedetails(request): return render(request, 'core/machinedetails.html')
 def assign_tech(request): return render(request, 'core/assign-tech.html')
 def raise_ticket(request): return render(request, 'core/raise-ticket.html')
+def machine_detail(request, machine_id): return render(request, 'core/machinedetails.html', {'id': machine_id})
 
 
 
